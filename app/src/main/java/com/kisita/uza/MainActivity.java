e229e815b1386b07b3cb585cab9077831e9a66fb
Hugues Kisita <Hugues Kisita>
2017-04-24 22:25:43 +0200

    add the drawer icon the toolbar

--- app/src/main/java/com/kisita/uza/MainActivity.java
+++ app/src/main/java/com/kisita/uza/MainActivity.java
@@ -15,7 +15,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -88,6 +87,9 @@ public class MainActivity extends CustomActivity
 		toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
 		setSupportActionBar(toolbar);
 
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+
 		setupDrawer();
 		setupContainer(2);
 
@@ -114,6 +116,7 @@ public class MainActivity extends CustomActivity
 			{
 			}
 		};
+		drawerToggle.syncState();
 		drawerLayout.setDrawerListener(drawerToggle);
 		drawerLayout.closeDrawers();
 
