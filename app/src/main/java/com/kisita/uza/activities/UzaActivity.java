0b74fd2f32a487eaff5132824d1e784558c42b34
Hugues Kisita <kisita2002@yahoo.fr>
2017-09-04 08:18:52 +0200

    Remove payment process from this fragment and handle amount to pay

--- app/src/main/java/com/kisita/uza/activities/UzaActivity.java
+++ app/src/main/java/com/kisita/uza/activities/UzaActivity.java
@@ -1,33 +1,71 @@
 package com.kisita.uza.activities;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.Toast;
 
+/*import com.braintreepayments.api.BraintreePaymentActivity;
+import com.braintreepayments.api.PaymentRequest;*/
+import com.braintreepayments.api.BraintreePaymentActivity;
+import com.braintreepayments.api.PaymentRequest;
+/*import com.braintreepayments.api.dropin.DropInActivity;
+import com.braintreepayments.api.dropin.DropInRequest;
+import com.braintreepayments.api.dropin.DropInResult;*/
+import com.braintreepayments.api.models.PaymentMethodNonce;
 import com.kisita.uza.R;
 import com.kisita.uza.custom.CustomActivity;
 import com.kisita.uza.model.Data;
 import com.kisita.uza.model.UzaListItem;
+import com.kisita.uza.services.Token;
 import com.kisita.uza.ui.CheckoutFragment;
+import com.kisita.uza.ui.CommandsFragment;
 import com.kisita.uza.ui.DetailFragment;
 import com.kisita.uza.ui.FavoritesFragment;
+import com.kisita.uza.ui.ItemsFragment;
 import com.kisita.uza.ui.PaymentMethodsFragment;
 import com.kisita.uza.ui.SettingsFragment;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import cz.msebera.android.httpclient.Header;
 
 import static com.kisita.uza.model.Data.UZA.NAME;
 
-public class UzaActivity extends CustomActivity implements CheckoutFragment.OnFragmentInteractionListener,PaymentMethodsFragment.OnListFragmentInteractionListener{
-    /** The toolbar. */
+public class UzaActivity extends CustomActivity implements CheckoutFragment.OnCheckoutInteractionListener,
+        PaymentMethodsFragment.OnPaymentListFragmentInteractionListener, ItemsFragment.OnItemFragmentInteractionListener {
+    /**
+     * The toolbar.
+     */
     public Toolbar toolbar;
+
+    private static final int REQUEST_CODE = Menu.FIRST;
+
+    private String mAmount = "0.0";
+
+    private ArrayList<Data> commandsToUpdate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
         toolbar = (Toolbar) findViewById(R.id.checkout_toolbar);
 
-        setFragment(getIntent().getIntExtra("fragment",-1));
+        setFragment(getIntent().getIntExtra("fragment", -1));
 
         setSupportActionBar(toolbar);
 
@@ -40,35 +78,34 @@ public class UzaActivity extends CustomActivity implements CheckoutFragment.OnFr
     }
 
 
-    void setFragment(int fid){
+    void setFragment(int fid) {
         String title = "";
         Fragment f = null;
-        switch (fid){
-            case(0):
+        switch (fid) {
+            case (0):
                 title = getString(R.string.favourites);
                 f = new FavoritesFragment();
                 break;
-            case(1):
+            case (1):
                 title = getString(R.string.checkout);
                 f = new CheckoutFragment();
                 break;
-            case(2):
+            case (2):
                 title = getString(R.string.settings);
                 f = new SettingsFragment();
                 break;
-            case(3):
+            case (3):
                 //title = getIntent().getStringArrayExtra("details")[NAME];
-                f = DetailFragment.newInstance((Data)getIntent().getSerializableExtra("details"));
+                f = DetailFragment.newInstance((Data) getIntent().getSerializableExtra("details"));
                 break;
-            /*case(4):
-                title = "My store";
-                f = new SellingFragment();
-                break;*/
+            case(4):
+                f =  CommandsFragment.newInstance("",false);
+                break;
             default:
                 break;
         }
         toolbar.setTitle(title);
-        if(f != null) {
+        if (f != null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.content_frame, f)
                     .commit();
@@ -83,19 +120,123 @@ public class UzaActivity extends CustomActivity implements CheckoutFragment.OnFr
     }
 
     @Override
-    public void onFragmentInteraction() {
+    public void onCheckoutInteraction(String amount, ArrayList<Data> commands) {
+        commandsToUpdate = (ArrayList<Data>) commands.clone();
         // Call from Checkout fragment
-        toolbar.setTitle("Payment method");
-        PaymentMethodsFragment f = PaymentMethodsFragment.newInstance(1 /*Number of column count*/);
+        toolbar.setTitle(R.string.payment_mthod);
+        PaymentMethodsFragment f = PaymentMethodsFragment.newInstance(1 /*Number of column count*/, amount);
         getSupportFragmentManager().beginTransaction()
-                .setCustomAnimations(R.anim.slide_in_right,R.anim.slide_out_left,R.anim.slide_in_left,R.anim.slide_out_right)
+                .setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left, R.anim.slide_in_left, R.anim.slide_out_right)
                 .addToBackStack(null)
                 .replace(R.id.content_frame, f)
                 .commit();
     }
 
     @Override
-    public void onListFragmentInteraction(UzaListItem item) {
-        //Data from PaymentMethods adapter
+    public void onPayementListFragmentInteraction(UzaListItem item, String amount) {
+        mAmount = amount;
+        switch (item.name) {
+            case R.string.credit_card:
+                paymentRequest();
+                break;
+            case R.string.cash:
+                break;
+            case R.string.mobile_money:
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    void paymentRequest() {
+        Log.i(TAG, "Launch braintree activity");
+        SharedPreferences sharedPref = getSharedPreferences(getResources().getString(R.string.uza_keys),
+                Context.MODE_PRIVATE);
+        String token = sharedPref.getString("braintree_token", null);
+        if (token == null) {
+            Toast.makeText(getApplicationContext(), getString(R.string.failed_to_get_token), Toast.LENGTH_LONG).show();
+            //TODO Retry here ?
+            return;
+        }
+        Log.i(TAG, "Token that will be used is  : " + token);
+
+
+        // TODO convert into USD
+        PaymentRequest paymentRequest = new PaymentRequest()
+                .clientToken(token)
+                .amount(mAmount)
+                .androidPayPhoneNumberRequired(true)
+                .primaryDescription(getString(R.string.secure_payement))
+                .secondaryDescription(getString(R.string.app_name))
+                .actionBarTitle(getString(R.string.payement))
+                .androidPayShippingAddressRequired(true)
+
+                .submitButtonText(getString(R.string.pay));
+        startActivityForResult(paymentRequest.getIntent(this), REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i(TAG, "received result is  : " + resultCode);
+        showProgressDialog(getString(R.string.please_wait));
+        if (resultCode == BraintreePaymentActivity.RESULT_OK) {
+            PaymentMethodNonce paymentMethodNonce = data.getParcelableExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
+            AsyncHttpClient client = new AsyncHttpClient();
+            Log.i("Checkout result", paymentMethodNonce.getNonce());
+
+            RequestParams requestParams = new RequestParams();
+            requestParams.put("payment_method_nonce", paymentMethodNonce.getNonce());
+            requestParams.put("amount", mAmount);
+
+            client.post(Token.BRAINTREE_SERVER, requestParams, new TextHttpResponseHandler() {
+                @Override
+                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                    Toast.makeText(getBaseContext(), R.string.fail_to_contact_payment_server, Toast.LENGTH_LONG).show();
+                    hideProgressDialog();
+                    onBackPressed();
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                    Toast.makeText(getBaseContext(), responseString, Toast.LENGTH_LONG).show();
+                    Log.i(TAG, responseString);
+                    if(responseString.equalsIgnoreCase("OK")){
+                        // Change command state
+                        setCommandsState();
+                        toolbar.setTitle(R.string.commands);
+                        // Go to the fragment that shows commands in processing
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.content_frame, CommandsFragment.newInstance("Men",false))
+                                .commit();
+                    }else{
+                        Toast.makeText(getBaseContext(), R.string.transaction_rejected, Toast.LENGTH_LONG).show();
+                    }
+                    hideProgressDialog();
+                }
+            });
+        }else if(resultCode == RESULT_CANCELED){
+            hideProgressDialog();
+        }else{
+            Toast.makeText(getBaseContext(), R.string.fail_to_contact_payment_server, Toast.LENGTH_LONG).show();
+            hideProgressDialog();
+        }
+    }
+
+    @Override
+    public void onItemFragmentInteraction(Uri uri) {
+
+    }
+
+
+    private void setCommandsState(){
+        Map<String, Object> commandsUpdates = new HashMap<>();
+        if(commandsToUpdate != null){
+            for(Data d : commandsToUpdate) {
+                commandsUpdates.put("/users-data/" + getUid() + "/commands/" + d.getKey() + "/state","1"); // 1 = command received
+                commandsUpdates.put("/commands/" + d.getKey() + "/state", "1");
+            }
+            getDb().updateChildren(commandsUpdates);
+        }
     }
 }
