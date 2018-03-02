e5f000365b28ee97785734261f074f30232ef856
Hugues Kisita <kisita2002@yahoo.fr>
2018-03-02 16:01:03 +0100

    Adding blink effect on splash screen

--- app/src/main/java/com/kisita/uza/activities/SplashScreen.java
+++ app/src/main/java/com/kisita/uza/activities/SplashScreen.java
@@ -1,11 +1,15 @@
 package com.kisita.uza.activities;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.widget.ImageView;
 
 import com.kisita.uza.R;
+
 
 /**
  * The Class SplashScreen will launched at the start of the application. It will
@@ -18,6 +22,7 @@ public class SplashScreen extends Activity
 	/** Check if the app is running. */
 	private boolean isRunning;
 
+	private ImageView ladyImg;
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
@@ -27,16 +32,27 @@ public class SplashScreen extends Activity
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
+		ladyImg = (ImageView) findViewById(R.id.lady) ;
 
 		isRunning = true;
+
+		manageBlink();
 
 		startSplash();
 
 	}
 
+	private void manageBlink(){
+        ObjectAnimator anim = ObjectAnimator.ofFloat(ladyImg,"alpha",0.2f);
+        anim.setDuration(1500);
+        anim.setRepeatMode(ValueAnimator.REVERSE);
+        anim.setRepeatCount(ValueAnimator.INFINITE);
+        anim.start();
+	}
+
 	/**
 	 * Starts the count down timer for 3-seconds. It simply sleeps the thread
-	 * for 3-seconds.
+	 * for 6-seconds.
 	 */
 	private void startSplash()
 	{
@@ -49,7 +65,7 @@ public class SplashScreen extends Activity
 				try
 				{
 
-					Thread.sleep(3000);
+					Thread.sleep(6000);
 
 				} catch (Exception e)
 				{
