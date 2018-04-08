bf17b1abec8da327ec7b0085950fcc7dc094c5a9
Hugues Kisita <kisita2002@yahoo.fr>
2018-04-08 09:30:14 +0200

    Handle back button action in MainActivity

--- app/src/main/java/com/kisita/uza/activities/MainActivity.java
+++ app/src/main/java/com/kisita/uza/activities/MainActivity.java
@@ -1,24 +1,35 @@
 package com.kisita.uza.activities;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.braintreepayments.api.dropin.DropInRequest;
 import com.kisita.uza.R;
 import com.kisita.uza.custom.CustomActivity;
+import com.kisita.uza.model.Data;
+import com.kisita.uza.services.Token;
 import com.kisita.uza.ui.CheckoutFragment;
 import com.kisita.uza.ui.ChoicesFragment;
 import com.kisita.uza.ui.FavoritesFragment;
 import com.kisita.uza.ui.OnSaleFragment;
 import com.kisita.uza.ui.SettingsFragment;
 
+import java.util.ArrayList;
 import java.util.EmptyStackException;
 import java.util.Stack;
 
-public class MainActivity extends CustomActivity {
+public class MainActivity extends CustomActivity implements CheckoutFragment.OnCheckoutInteractionListener {
 
     private static final String CURRENT_FRAGMENT_ID = "current_fragment_id";
     private Fragment fragment = OnSaleFragment.newInstance();
@@ -31,13 +42,23 @@ public class MainActivity extends CustomActivity {
 
     private boolean mEmptyingStack = false;
 
+    private static final int REQUEST_CODE = 100;
+
+    private String mAuthorization;
+
+    // Alarm manager
+
+    public static final long ALARM_TRIGGER_AT_TIME = SystemClock.elapsedRealtime(); // Start now
+    public static final long ALARM_INTERVAL = AlarmManager.INTERVAL_HALF_DAY ;
+
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            setFragment(setCurrentFragment(item.getItemId()));
-            Log.i(TAG,"onNavigationItemSelected");
+            if(item.getItemId() != mCurrentFragmentId)
+                setFragment(setCurrentFragment(item.getItemId()));
             return true;
         }
     };
@@ -72,7 +93,7 @@ public class MainActivity extends CustomActivity {
         }
 
         if(!mEmptyingStack) {
-            mFragmentsStack.push(fragmentId);
+            mFragmentsStack.push(mCurrentFragmentId);
             Log.i(TAG,"Stack : "+ mFragmentsStack);
         }else{
             mEmptyingStack = false;
@@ -94,6 +115,7 @@ public class MainActivity extends CustomActivity {
         }
 
         setFragment(setCurrentFragment(mCurrentFragmentId));
+        setAlarmManager();
     }
 
     public void setFragment(Fragment f){
@@ -127,5 +149,35 @@ public class MainActivity extends CustomActivity {
         }catch(EmptyStackException e){
             finish();
         }
+    }
+
+    private void setAlarmManager(){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Intent tokenIntent = new Intent(this, Token.class);
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, tokenIntent, 0);
+        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                ALARM_TRIGGER_AT_TIME,ALARM_INTERVAL, pendingIntent);
+    }
+
+    void paymentRequest() {
+        Log.i(TAG, "Launch Payment activity");
+        SharedPreferences sharedPref = getSharedPreferences(getResources().getString(R.string.uza_keys),
+                Context.MODE_PRIVATE);
+        String token = sharedPref.getString("braintree_token", null);
+        if (token == null) {
+            Toast.makeText(getApplicationContext(), getString(R.string.failed_to_get_token), Toast.LENGTH_LONG).show();
+            //TODO Retry here ?
+            return;
+        }
+
+
+        DropInRequest dropInRequest = new DropInRequest()
+                .clientToken(token);
+        startActivityForResult(dropInRequest.getIntent(this), REQUEST_CODE);
+    }
+
+    @Override
+    public void onCheckoutInteraction(String amount, ArrayList<Data> commands) {
+        paymentRequest();
     }
 }
