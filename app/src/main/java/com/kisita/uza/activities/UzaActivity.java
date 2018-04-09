6d99e2270f1f50f1d4eec3ce4de0803a159a5f34
Hugues Kisita <kisita2002@yahoo.fr>
2018-04-09 09:39:28 +0200

    Move payment processing from UzaActivity to MainActivity

--- app/src/main/java/com/kisita/uza/activities/UzaActivity.java
+++ app/src/main/java/com/kisita/uza/activities/UzaActivity.java
@@ -1,24 +1,17 @@
 package com.kisita.uza.activities;
 
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.widget.Toast;
-import com.braintreepayments.api.BraintreePaymentActivity;
-import com.braintreepayments.api.PaymentRequest;
-import com.braintreepayments.api.models.PaymentMethodNonce;
 import com.facebook.CallbackManager;
 import com.kisita.uza.R;
 import com.kisita.uza.custom.CustomActivity;
 import com.kisita.uza.model.Data;
-import com.kisita.uza.services.Token;
 import com.kisita.uza.ui.BillingFragment;
 import com.kisita.uza.ui.CheckoutFragment;
 import com.kisita.uza.ui.DetailFragment;
@@ -26,13 +19,9 @@ import com.kisita.uza.ui.FavoritesFragment;
 import com.kisita.uza.ui.ItemsFragment;
 import com.kisita.uza.ui.MapsFragment;
 import com.kisita.uza.ui.SettingsFragment;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.RequestParams;
-import com.loopj.android.http.TextHttpResponseHandler;
+
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import cz.msebera.android.httpclient.Header;
+
 
 
 public class UzaActivity extends CustomActivity implements CheckoutFragment.OnCheckoutInteractionListener,
@@ -42,10 +31,6 @@ public class UzaActivity extends CustomActivity implements CheckoutFragment.OnCh
      */
     public Toolbar toolbar;
 
-    private static final int REQUEST_CODE = Menu.FIRST;
-
-    private String mAmount = "0.0";
-
     private CallbackManager callbackManager;
 
 
@@ -53,7 +38,7 @@ public class UzaActivity extends CustomActivity implements CheckoutFragment.OnCh
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
-        toolbar = (Toolbar) findViewById(R.id.checkout_toolbar);
+        toolbar = findViewById(R.id.checkout_toolbar);
 
         setFragment(getIntent().getIntExtra("fragment", -1));
 
@@ -74,23 +59,19 @@ public class UzaActivity extends CustomActivity implements CheckoutFragment.OnCh
         Fragment f = null;
         switch (fid) {
             case (0):
-                title = getString(R.string.favourites);
                 f = new FavoritesFragment();
                 break;
             case (1):
-                title = getString(R.string.checkout);
                 f = new CheckoutFragment();
                 break;
             case (2):
-                title = getString(R.string.settings);
                 f = new SettingsFragment();
                 break;
             case (3):
-                //title = getIntent().getStringArrayExtra("details")[NAME];
                 f = DetailFragment.newInstance((Data) getIntent().getSerializableExtra("details"));
                 break;
             case(4):
-                toolbar.setTitle(R.string.commands);
+                title=getString(R.string.billing_information);
                 f = BillingFragment.newInstance();
                 break;
             default:
@@ -116,43 +97,10 @@ public class UzaActivity extends CustomActivity implements CheckoutFragment.OnCh
     }
 
 
-
-    void paymentRequest() {
-        Log.i(TAG, "Launch braintree activity");
-        SharedPreferences sharedPref = getSharedPreferences(getResources().getString(R.string.uza_keys),
-                Context.MODE_PRIVATE);
-        String token = sharedPref.getString("braintree_token", null);
-        if (token == null) {
-            Toast.makeText(getApplicationContext(), getString(R.string.failed_to_get_token), Toast.LENGTH_LONG).show();
-            //TODO Retry here ?
-            return;
-        }
-
-        PaymentRequest paymentRequest = new PaymentRequest()
-                .clientToken(token)
-                .amount(mAmount)
-                .androidPayPhoneNumberRequired(true)
-                .primaryDescription(getString(R.string.secure_payement))
-                .secondaryDescription(getString(R.string.app_name))
-                .actionBarTitle(getString(R.string.payement))
-                .androidPayShippingAddressRequired(true)
-
-                .submitButtonText(getString(R.string.pay));
-        startActivityForResult(paymentRequest.getIntent(this), REQUEST_CODE);
-    }
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.i(TAG, "received result is  : " + resultCode + " request code : "+requestCode);
-        //showProgressDialog(getString(R.string.please_wait));
-        switch(requestCode){
-            case REQUEST_CODE:
-                handlePaymentResult(resultCode, data);
-                break;
-            default:
-                handleFacebookResult(requestCode,resultCode, data);
-        }
-
+        handleFacebookResult(requestCode,resultCode, data);
     }
 
     private void handleFacebookResult(int requestCode,int resultCode, Intent data) {
@@ -161,53 +109,11 @@ public class UzaActivity extends CustomActivity implements CheckoutFragment.OnCh
         hideProgressDialog();
     }
 
-    private void handlePaymentResult(int resultCode, Intent data) {
-        if (resultCode == BraintreePaymentActivity.RESULT_OK) {
-            PaymentMethodNonce paymentMethodNonce = data.getParcelableExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
-            AsyncHttpClient client = new AsyncHttpClient();
-            Log.i("Checkout result", paymentMethodNonce.getNonce());
-
-            RequestParams requestParams = new RequestParams();
-            requestParams.put("payment_method_nonce", paymentMethodNonce.getNonce());
-            requestParams.put("amount", mAmount);
-
-            client.post(Token.BRAINTREE_SERVER, requestParams, new TextHttpResponseHandler() {
-                @Override
-                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                    Toast.makeText(getBaseContext(), R.string.fail_to_contact_payment_server, Toast.LENGTH_LONG).show();
-                    hideProgressDialog();
-                    onBackPressed();
-                }
-
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                    Toast.makeText(getBaseContext(), responseString, Toast.LENGTH_LONG).show();
-                    Log.i(TAG, responseString);
-                    if(responseString.equalsIgnoreCase("OK")){
-                        // Change command state
-                        // Go to the fragment that shows commands in processing
-                        /*getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.content_frame, CommandsFragment.newInstance("",false),"COMMANDS")
-                                .commit();*/
-                    }else{
-                        Toast.makeText(getBaseContext(), R.string.transaction_rejected, Toast.LENGTH_LONG).show();
-                    }
-                    hideProgressDialog();
-                }
-            });
-        }else if(resultCode == RESULT_CANCELED){
-            hideProgressDialog();
-        }else{
-            Toast.makeText(getBaseContext(), R.string.fail_to_contact_payment_server, Toast.LENGTH_LONG).show();
-            hideProgressDialog();
-        }
-    }
-
     @Override
     public void onItemFragmentInteraction(String title) {
         //Log.i(TAG,"Reload fragment with tag : "+title);
         // Reload current fragment
-		Fragment frg = null;
+		Fragment frg;
 		frg = getSupportFragmentManager().findFragmentByTag(title);
 		final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 		ft.detach(frg);
