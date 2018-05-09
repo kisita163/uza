2ebb9af4dce2bbef6489574f1b7c0062dbd378eb
Hugues Kisita <kisita2002@yahoo.fr>
2018-05-09 11:39:37 +0200

    Avoid illegal state exception when comitting fragments

--- app/src/main/java/com/kisita/uza/activities/DrawerActivity.java
+++ app/src/main/java/com/kisita/uza/activities/DrawerActivity.java
@@ -434,20 +434,11 @@ public class DrawerActivity extends CustomActivity
         setFragment(setCurrentFragment( mFragmentsStack.peek()));
     }
 
-    /**
-     * Dispatch onResume() to fragments.  Note that for better inter-operation
-     * with older versions of the platform, at the point of this call the
-     * fragments attached to the activity are <em>not</em> resumed.  This means
-     * that in some cases the previous state may still be saved, not allowing
-     * fragment transactions that modify the state.  To correctly interact
-     * with fragments in their proper state, you should instead override
-     * {@link #onResumeFragments()}.
-     */
     @Override
-    protected void onResume() {
+    protected void onResumeFragments() {
         BiLog.i(TAG,"Resume items here" + itemsList.size());
         notifyChanges();
-        super.onResume();
+        super.onResumeFragments();
     }
 
     private ArrayList<Data> getInCartItems(){
